36b3469d7509443ca3f7e3a1cdb72165676c0d76
Julian Sedano <ignovigenz@hotmail.com>
2026-04-03 01:08:56 +0000

    models User and Admin created

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,34 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Admin(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    
 
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nickname: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nickname": self.nickname,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
