8804b6f68c8bbff76ecb46587f7d6afbd1d2af09
Julian Sedano <ignovigenz@hotmail.com>
2026-04-20 01:57:54 +0000

    Render

--- src/app.py
+++ src/app.py
@@ -77,8 +77,8 @@ def serve_any_other_file(path):
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    PORT = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
 
 @jwt.invalid_token_loader
 def invalid_token_callback(error):
